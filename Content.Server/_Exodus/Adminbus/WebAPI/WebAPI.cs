b71807e74b1bc6578bf731677330741f48b0c4ea
Maxim Bychkov <60302610+Lokilife@users.noreply.github.com>
2026-04-12 13:38:25 +0300

    WebAPI Shutdown Endpoint (#216)
    * WebAPI Shutdown Endpoint
    
    * sec

--- Content.Server/_Exodus/Adminbus/WebAPI/WebAPI.cs
+++ Content.Server/_Exodus/Adminbus/WebAPI/WebAPI.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Content.Server.RoundEnd;
 using Content.Shared._Exodus.CCVar;
 using JetBrains.Annotations;
+using Robust.Server;
 using Robust.Server.ServerStatus;
 using Robust.Shared;
 using Robust.Shared.Asynchronous;
@@ -21,6 +22,7 @@ public sealed partial class WebAPI : IPostInjectInit
     [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly ITaskManager _task = default!;
     [Dependency] private readonly IEntityManager _entity = default!;
+    [Dependency] private readonly IBaseServer _server = default!;
 
     private string? _webapiToken;
     private ISawmill _sawmill = default!;
@@ -44,6 +46,7 @@ public sealed partial class WebAPI : IPostInjectInit
         _sawmill = Logger.GetSawmill("exds.webapi");
 
         _statusHost.AddHandler(RequestRoundEnd);
+        _statusHost.AddHandler(RequestShutdown);
     }
 
     private async Task<bool> RequestRoundEnd(IStatusHandlerContext context)
@@ -55,7 +58,7 @@ public sealed partial class WebAPI : IPostInjectInit
 
         if (_webapiToken == null)
         {
-            _sawmill.Warning("Watchdog token is unset but received POST /endround API call. Ignoring");
+            _sawmill.Warning("WebAPI token is unset but received POST /endround API call. Ignoring");
             return false;
         }
 
@@ -63,8 +66,6 @@ public sealed partial class WebAPI : IPostInjectInit
 
         if (auth != _webapiToken)
         {
-            // Holy shit nobody read these logs please.
-            _sawmill.Verbose(@"Failed auth: ""{0}"" vs ""{1}""", auth, _webapiToken);
             await context.RespondErrorAsync(HttpStatusCode.Unauthorized);
             return true;
         }
@@ -103,11 +104,70 @@ public sealed partial class WebAPI : IPostInjectInit
         return true;
     }
 
+    private async Task<bool> RequestShutdown(IStatusHandlerContext context)
+    {
+        if (context.RequestMethod != HttpMethod.Post || context.Url.AbsolutePath != "/webapi/shutdown")
+        {
+            return false;
+        }
+
+        if (_webapiToken == null)
+        {
+            _sawmill.Warning("WebAPI token is unset but received POST /shutdown API call. Ignoring");
+            return false;
+        }
+
+        var auth = context.RequestHeaders["WebAPIToken"];
+
+        if (auth != _webapiToken)
+        {
+            await context.RespondErrorAsync(HttpStatusCode.Unauthorized);
+            return true;
+        }
+
+        RequestShutdownParams? parameters = null;
+        if (context.RequestHeaders.TryGetValue("Content-Type", out var contentType)
+            && contentType == MediaTypeNames.Application.Json)
+        {
+            try
+            {
+                parameters = await context.RequestBodyJsonAsync<RequestShutdownParams>();
+            }
+            catch (JsonException)
+            {
+                // parameters null so it'll catch the block down below.
+            }
+
+            if (parameters == null)
+            {
+                await context.RespondErrorAsync(HttpStatusCode.BadRequest);
+                return true;
+            }
+        }
+
+        parameters ??= new RequestShutdownParams();
+
+        _task.RunOnMainThread(() =>
+        {
+            _server.Shutdown(parameters.Reason);
+        });
+
+        await context.RespondAsync("Success", HttpStatusCode.OK);
+
+        return true;
+    }
+
     [UsedImplicitly]
     public sealed class RequestRoundEndParams
     {
         public TimeSpan CountdownTime { get; set; } = TimeSpan.FromMinutes(10);
         public string Text { get; set; } = "round-end-system-shuttle-called-announcement";
         public string Name { get; set; } = "round-end-system-shuttle-sender-announcement";
+    }
+
+    [UsedImplicitly]
+    public sealed class RequestShutdownParams
+    {
+        public string? Reason { get; set; } = null;
     }
 }
